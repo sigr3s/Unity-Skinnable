0361970f23d2eba6760a0e3ea65571d231c19540
Sigr3s <tortosa95@gmail.com>
2018-11-13 13:47:44 +0100

    Update gitignore and reordered demos to handle the correct assets.

--- Assets/Skinnable/Demos/Scripts/ToggleStyle.cs
+++ Assets/Skinnable/Demos/Scripts/ToggleStyle.cs
@@ -4,26 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ToggleStyle : BaseStyle<Toggle> {
-    public Color normalColor = Color.white;
-    public Color highlightedColor = Color.white;
-    public Color pressedColor = Color.white;
-    public Color disabledColor = Color.white;
+    public ColorBlock colorBlock;
 
-    public Sprite buttonSprite;
+    public Sprite toggleSprite;
 
     public override void ApplyStyle(ref System.Object targetObject)
     {
         Toggle toggle = targetObject as Toggle;
         if (toggle != null) {
-            ColorBlock cb = toggle.colors;
-            if(ShouldApply(nameof(normalColor)) ) cb.normalColor = normalColor;
-            if(ShouldApply(nameof(highlightedColor)) ) cb.highlightedColor = highlightedColor;
-            if(ShouldApply(nameof(pressedColor)) ) cb.pressedColor = pressedColor;
-            if(ShouldApply(nameof(disabledColor)) ) cb.disabledColor = disabledColor;
-            toggle.colors = cb;
-            if(ShouldApply(nameof(buttonSprite))){
+
+            if(ShouldApply(nameof(colorBlock))) toggle.colors = colorBlock;
+
+            if(ShouldApply(nameof(toggleSprite))){
                 Image image = toggle.targetGraphic as Image;
-                if(image != null) image.sprite = buttonSprite;
+                if(image != null) image.sprite = toggleSprite;
             }
         }
     }
